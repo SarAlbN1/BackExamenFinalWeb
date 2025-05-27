0ae47934e5c860ddad4d46c0bc5cf49285ed75d4
SarAlbN1 <sara.albarracin.ns@gmail.com>
2025-05-27 18:18:05 -0500

    Estructura inicial Spring Boot con H2 y JPA

---
+++ src/main/java/com/sara/backexamenfinalweb/BackexamenfinalwebApplication.java
@@ -0,0 +1,13 @@
+package com.sara.backexamenfinalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackexamenfinalwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackexamenfinalwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sara/backexamenfinalweb/BackexamenfinalwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.backexamenfinalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackexamenfinalwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
